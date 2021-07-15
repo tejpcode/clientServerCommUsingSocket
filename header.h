dfb8fff642a906fa556be1ac10a3ffb7982279de
tej <tej pratap>
2021-07-15 22:23:27 +0530

    contains all the required header file

---
+++ header.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<sys/sem.h>
+#include<pthread.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<signal.h>
+#include<semaphore.h>
+
